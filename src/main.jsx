e48e92211f44277107a85c8d278f305fde4a8d32
dheeraj kumar dwivedi <dheerajdwivediweb@gmail.com>
2026-03-31 15:30:45 +0530

    feat: integrate user registration and login forms with Redux and React Hook Form
    - Added new dependencies: @hookform/resolvers, @reduxjs/toolkit, react-hook-form, react-icons, react-redux, and zod.
    - Implemented user registration and login forms using React Hook Form and Zod for validation.
    - Created Redux slice for user registration with async thunk for API calls.
    - Configured Axios for API requests.
    - Added UI components for input fields and buttons.
    - Integrated social media login options.
    - Updated main application to use Redux store.
    - Added multiple assets including images and UI components.

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./context/store.js";
 
 createRoot(document.getElementById("root")).render(
-   <StrictMode>
-      <div className="bg-mint-500">
-         <App />
-      </div>
-   </StrictMode>
+   <Provider store={store}>
+      <App />
+   </Provider>
 );
